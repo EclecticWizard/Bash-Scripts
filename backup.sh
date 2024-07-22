940e9da593e45ae4d16b6d86d5f70c74fa0ac37f
EclecticWizard <hodgkinsj95@proton.me>
2024-07-22 15:23:37 +0100

    Gave up on linking Dropbox

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
-cd ~
-rsync -av Documents/NKS-Notes/ /mnt/share/vault_backups/
-rsync -av Documents/Obsidian/ /mnt/share/vault_backups/
-rsync -av Documents/Calibre\ Library/ /mnt/share/Calibre/
-rsync Documents /Dropbox/Backups/FedoraDocs/
+rsync -av ~/Documents/NKS-Notes/ /mnt/share/vault_backups/
+rsync -av ~/Documents/Obsidian/ /mnt/share/vault_backups/
+rsync -av ~/Documents/Calibre\ Library/ /mnt/share/Calibre/
+rsync -av ~/Documents/Qualifications/ /mnt/share/
