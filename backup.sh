928939a464b49c76fe01e11bcfb12e9f539247dd
EclecticWizard <hodgkinsj95@proton.me>
2024-07-22 15:13:55 +0100

    initial commit

---
+++ backup.sh
@@ -0,0 +1,5 @@
+cd ~
+rsync -av Documents/NKS-Notes/ /mnt/share/vault_backups/
+rsync -av Documents/Obsidian/ /mnt/share/vault_backups/
+rsync -av Documents/Calibre\ Library/ /mnt/share/Calibre/
+rsync Documents /Dropbox/Backups/FedoraDocs/
