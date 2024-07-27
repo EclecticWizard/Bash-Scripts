5277859054b76ae3704c3daefe05fc334d03caa6
EclecticWizard <hodgkinsj95@proton.me>
2024-07-27 07:11:01 +0100

    copy files to dropbox

--- backup.sh
+++ backup.sh
@@ -6,12 +6,23 @@ src=(~/Documents/Obsidian/
 dest=(/mnt/share/vault_backups/
   /mnt/share/vault_backups/
   /mnt/share/Calibre/
-  /mnt/share/)
+  /mnt/share/Calibre/)
+
+dropbox=(~/Documents/Obsidian/
+  ~/Documents/NKS-Notes/)
 
 i=0
 while [ $i -lt ${#src[@]} ]; do
-  echo "[*] backing up ${src[i]}"
+  echo "--[*] backing up ${src[i]}"
   rsync -av ${src[i]} ${dest[i]}
+  ((i++))
+done
+
+echo "[*]Manually copying files for Dropbox"
+i=0
+while [ $i -lt ${#dropbox[@]} ]; do
+  echo "--[*] Copying ${dropbox[i]}"
+  cp -r ${dropbox[i]} ~/Dropbox/Obsidian/
   ((i++))
 done
 
